6a115319a0e9e7ff13165d2e82668e4862ddebd3
arahansa <arahansa@naver.com>
2017-03-14 18:35:31 +0900

    first commit

---
+++ src/com/arahansa/TestJitPackClass.java
@@ -0,0 +1,10 @@
+package com.arahansa;
+
+/**
+ * Created by lucas.choi on 2017. 3. 14..
+ */
+public class TestJitPackClass {
+    public static String hello(){
+        return "Hello jitpack";
+    }
+}
